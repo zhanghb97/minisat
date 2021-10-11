d227f62e13a97b55f2a5eb1a35d10a3b05f62c5a
zhanghb97 <hongbin2019@iscas.ac.cn>
2021-10-11 10:15:23 +0800

    Fix friend declaration with default arguments.

--- minisat/core/SolverTypes.h
+++ minisat/core/SolverTypes.h
@@ -47,12 +47,14 @@ typedef int Var;
   const Var var_Undef = -1;
 #endif
 
+struct Lit;
+Lit mkLit(Var var, bool sign = false);
 
 struct Lit {
     int     x;
 
     // Use this as a constructor:
-    friend Lit mkLit(Var var, bool sign = false);
+    friend Lit mkLit(Var var, bool sign);
 
     bool operator == (Lit p) const { return x == p.x; }
     bool operator != (Lit p) const { return x != p.x; }
